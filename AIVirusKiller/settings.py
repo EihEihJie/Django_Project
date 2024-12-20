172a71a8cb3bbc3982ff412c7a4d7c45d2086440
eiheihjie <2606878727@qq.com>
2024-12-20 10:22:26 +0800

    "创建子应用，并注册"

--- AIVirusKiller/settings.py
+++ AIVirusKiller/settings.py
@@ -34,8 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.o.contrib.messages',contrib.sessions',
-    'djang
+    'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.users.apps.UsersConfig',
 ]
