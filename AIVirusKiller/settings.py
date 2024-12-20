cb83d9744fa9485a18759249bee45f3483da5522
eiheihjie <2606878727@qq.com>
2024-12-20 10:39:08 +0800

    "创建子应用，并注册"

--- AIVirusKiller/settings.py
+++ AIVirusKiller/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+from apps import users
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.users.apps.UsersConfig',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
